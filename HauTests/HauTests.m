950a6b440b328281db253fd7ff1f7565605d0bb4
Andrew Horst <andrew.horst@gmail.com>
2014-07-06 13:13:21 -0700

    Initial Commit

---
+++ HauTests/HauTests.m
@@ -0,0 +1,34 @@
+//
+//  HauTests.m
+//  HauTests
+//
+//  Created by Andrew on 7/6/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HauTests : XCTestCase
+
+@end
+
+@implementation HauTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
